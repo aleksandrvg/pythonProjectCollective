ccc82b93d0833a1cd0fbca0621cb47ee9e479f43
User <aleksandrvg79@gmail.com>
2023-12-27 13:29:55 +0300

    #2 Написана функция Reverse

--- main.py
+++ main.py
@@ -6,15 +6,14 @@ def Reverse(arr: list) -> list:
     reversed_list = []
     for i in range(len(arr) - 1, -1, -1):
         reversed_list.append(arr[i])
-    print(reversed_list)
 
 
 if __name__ == "__main__":
     mass = [1, 2, 9, 3, 8, 23, 9, 228, 3]
-    mass = BoubleSort(mass)
+    mass1 = BoubleSort(mass)
     mass2 = Reverse(mass)
     mass.reverse()
-    if mass == mass2:
+    if mass1 == mass2:
         print("Ok")
     else:
         print("Not Work")
